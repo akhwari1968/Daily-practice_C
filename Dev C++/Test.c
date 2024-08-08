70fa4af4edbcf6b9de9df57d7ed0cfa11031ee0f
king176769 <2630176769@qq.com>
2024-08-08 22:33:49 +0800

    8.8

--- Dev C++/Test.c
+++ Dev C++/Test.c
@@ -1,67 +1,46 @@
-#include <stdio.h>
-
-int conut(int x)
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+using namespace std;
+int n;
+string str;
+vector<int> add(vector<int> A, vector<int> B)
 {
-	int a = 0;
-	
-	while (x)
-	{
-		a++;
-		x /= 10;
-	}
-	
-	return a;
+    vector<int> C;
+    for (int i = 0, t = 0; i < A.size() || i < B.size() || t; ++ i )
+    {
+        if (i < A.size())
+            t += A[i];
+        if (i < B.size())
+            t += B[i];
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    return C;
 }
-
 int main()
 {
-	int n = 0;
-	int hao = 0;
-	scanf("%d",&n);
-	
-	for (int i = 1;i <= n;i++)
-	{
-		int tmp = i;
-		int a1 = conut(i);
-		int j = 0;
-		int wei = 1;
-		int t1 = 1;
-		int t2 = 1;
-		
-		for (j = 0;j < a1;j++)
-		{
-			if (wei % 2 != 0)
-			{
-				if ((tmp % 10) % 2 == 0)
-				{
-					t1 = 0;
-					break;
-				}
-				else
-				{
-					t1 = 1;
-				}
-			}
-			else if (wei % 2 == 0)
-			{
-				if ((tmp % 10) % 2 != 0)
-				{
-					t2 = 0;
-					break;
-				}
-				else
-				{
-					t2 = 1;
-				}
-			}
-			wei++;
-			tmp /= 10;
-		}
-		if (t1 == 1 && t2 == 1)
-		{
-			hao++;
-		}
-	}
-	
-	printf("%d",hao);
+    cin >> n >> str;
+    int k = str.size() - 1;
+    while (str[k] != '.')
+        k --;
+    str.erase(k, 1);
+    k = str.size() - k;
+    vector<int> A;
+    for (int i = str.size() - 1; i >= 0; -- i )
+        A.push_back(str[i] - '0');
+    while (n -- )
+        A = add(A, A);
+    int p = A[k - 1];
+    reverse(A.begin(), A.end());
+    while (k -- )
+        A.pop_back();
+    reverse(A.begin(), A.end());
+    if (p >= 5)
+        A = add(A, {1});
+    for (int i = A.size() - 1; i >= 0; -- i )
+        cout << A[i];
+    return 0;
 }
