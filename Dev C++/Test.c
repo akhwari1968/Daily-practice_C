d4d4a67d6897d81c670bf2259ece381b86d133a6
king176769 <2630176769@qq.com>
2024-08-21 23:18:14 +0800

    8.21

--- Dev C++/Test.c
+++ Dev C++/Test.c
@@ -2,7 +2,38 @@
 
 int main()
 {
-  printf("0%o 0x%x",123456789,123456789);
-  
-  return 0;
+	int n = 0;
+	int arr[100][100];
+	int row,column;
+	int z = 1;
+	int i = 0;
+	while (scanf("%d",&n) == 1 && n)
+	{
+		for (i = 0;i < n;i++)
+		{
+			row = i;
+			column = 0;
+			while (1)
+			{
+				arr[row][column] = z;
+				z++;
+				if (row == 0 && column == i)
+				{
+					break;
+				}
+				row--;
+				column++;
+			}
+		}
+		for (row = 0;row < n;row++)
+		{
+			for (column = 0;column < n-row;column++)
+			{
+				printf("%d ",arr[row][column]);
+			}
+			printf("\n");
+		}
+		}
+	
+	return 0;
 }
