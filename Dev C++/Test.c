3d668a56e090e606e7de06e96aaf103ee9c7f203
king176769 <2630176769@qq.com>
2024-09-11 14:36:09 +0800

    test

--- Dev C++/Test.c
+++ Dev C++/Test.c
@@ -3,32 +3,7 @@
 
 int main()
 {
-	char arr[101];
-	gets(arr);
-	
-	int left = 0;
-	int right = ((strlen(arr)) - 1);
-	int tmp = 0;
-	
-	while (1)
-	{
-		if (left == right || left > right)
-		{
-			break;
-		}
-		tmp = arr[left];
-		arr[left] = arr[right];
-		arr[right] = tmp;
-		
-		left++;
-		right--;
-	}
-	
-	int i = 0;
-	for (i = 0;i < strlen(arr);i++)
-	{
-		printf("%c",arr[i]);
-	}
+	printf("test");
 	
 	return 0;
 }
